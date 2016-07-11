578bf425ee8b5aba42f3e678c8635805a7e05e95
J4TZU <tatu.laun@gmail.com>
2016-07-11 22:56:59 +0300

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__ . '/../bootstrap/app.php';
+
+$app->run();
+
+?>
+<!doctype html>
+<html>
+
+    <head>
+        <title></title>
+        <!--<link rel="stylesheet" href="assets/css/all.css">-->
+        <script type="text/javascript" src="http:livejs.com/live.js"></script>
+    </head>
+    <body>
+        
+    </body>
+</html>
